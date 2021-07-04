e6408e529eb4be9bf7b021496a8948fb2aba3676
abigmiu <1552487872@qq.com>
2021-07-04 21:28:54 +0800

    day1 学习spring video1

---
+++ spring/src/main/java/Student.java
@@ -0,0 +1,10 @@
+
+import lombok.Data;
+
+
+@Data
+public class Student {
+    private Integer id;
+    private String name;
+    private Integer age;
+}
---
+++ spring/src/test/java/Test.java
@@ -0,0 +1,11 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        Student student = (Student) applicationContext.getBean("student");
+        System.out.println(student);
+    }
+}
