856ae855a93c70e766c7ae7fd5342b812f0de4b3
abigmiu <1552487872@qq.com>
2021-08-21 23:48:53 +0800

    mysql 视图， c创建，更新表

--- mysqlbzbh.sql
+++ mysqlbzbh.sql
@@ -133,50 +133,106 @@ SELECT
 	prod_price 
 FROM
 	vendors
-	INNER JOIN products ON vendors.vend_id = products.vend_id;
-# 自联结
+	INNER JOIN products ON vendors.vend_id = products.vend_id;# 自联结
 ## 子查询
-select prod_id, prod_name from products where vend_id = (select vend_id from products where prod_id = 'dtntr')
-
-select vend_id from products where prod_id = 'dtntr'
-select prod_id, prod_name from products where vend_id = 1003
-
-select p1.prod_id, p1.prod_name from products as p1, products as p2 where p1.vend_id = p2.vend_id and p2.prod_id = 'dtntr'
-
-select customers.cust_id, orders.order_num from customers inner join orders on customers.cust_id = orders.cust_id;
-
-# 组合查询
-select vend_id, prod_id, prod_name from products where prod_price <= 5 union select vend_id, prod_id, prod_price from products where vend_id in (1001, 1002)
-
-# 全文搜索
-select note_text from productnotes where MAtch(note_text) Against('rabbit')
-
-# 插入
+SELECT
+	prod_id,
+	prod_name 
+FROM
+	products 
+WHERE
+	vend_id = ( SELECT vend_id FROM products WHERE prod_id = 'dtntr' ) SELECT
+	vend_id 
+FROM
+	products 
+WHERE
+	prod_id = 'dtntr' SELECT
+	prod_id,
+	prod_name 
+FROM
+	products 
+WHERE
+	vend_id = 1003 SELECT
+	p1.prod_id,
+	p1.prod_name 
+FROM
+	products AS p1,
+	products AS p2 
+WHERE
+	p1.vend_id = p2.vend_id 
+	AND p2.prod_id = 'dtntr' SELECT
+	customers.cust_id,
+	orders.order_num 
+FROM
+	customers
+	INNER JOIN orders ON customers.cust_id = orders.cust_id;# 组合查询
+SELECT
+	vend_id,
+	prod_id,
+	prod_name 
+FROM
+	products 
+WHERE
+	prod_price <= 5 UNION
+SELECT
+	vend_id,
+	prod_id,
+	prod_price 
+FROM
+	products 
+WHERE
+	vend_id IN ( 1001, 1002 ) # 全文搜索
+SELECT
+	note_text 
+FROM
+	productnotes 
+WHERE
+	MATCH ( note_text ) Against ( 'rabbit' ) # 插入
 ## 自增id插入null不安全
-insert into customers values(null, '1','1','1','1','1',null, null,null)
-## 安全做法
-insert into customers(cust_name, cust_address, cust_city, cust_state, cust_zip, cust_country, cust_contact, cust_email) values('pen', '100', 'los', 'ca', '90046', 'usa', null, null)
+	INSERT INTO customers
+VALUES
+	( NULL, '1', '1', '1', '1', '1', NULL, NULL, NULL ) ## 安全做法
+	INSERT INTO customers ( cust_name, cust_address, cust_city, cust_state, cust_zip, cust_country, cust_contact, cust_email )
+VALUES
+	( 'pen', '100', 'los', 'ca', '90046', 'usa', NULL, NULL ) ## 插入多行
+	INSERT INTO customers ( cust_name, cust_address, cust_city, cust_state, cust_zip, cust_country )
+VALUES
+	( 'pep', '100', 'los', 'ca', '90046', 'usa' ),
+	( 'pep', '100', 'los', 'ca', '90046', 'usa' )
+	
+# 更新和删除数据
+UPDATE customers 
+SET cust_email = 'abc@cba.com' 
+WHERE
+	cust_id = 10005
+	
+UPDATE customers 
+SET cust_name = 'abc', cust_email = 'sdasd@gsdgsd.com' 
+WHERE
+	cust_id = 10005
+	
+DELETE 
+FROM
+	customers 
+WHERE
+	cust_id = 10006
+	
+# 创建和更新表
+ALTER TABLE vendors ADD vend_phone CHAR (
+26)
 
-## 插入多行
-insert into customers(cust_name,
-cust_address,
-	cust_city,
-	cust_state,
-	cust_zip,
-	cust_country
-) values (
-	'pep',
-	'100',
-	'los',
-	'ca',
-	'90046',
-	'usa'
-), (
-	'pep',
-	'100',
-	'los',
-	'ca',
-	'90046',
-	'usa'
-)
-
+# 视图
+CREATE VIEW productcustomers AS SELECT
+cust_name,
+cust_contact,
+prod_id 
+FROM
+	customers,
+	orders,
+	orderitems 
+WHERE
+	customers.cust_id = orders.cust_id 
+	AND orderitems.order_num = orders.order_num
+	
+	
+select * from productcustomers
