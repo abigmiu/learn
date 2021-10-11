fcffa9e787067c25bb9d57d369936c3e36cdf6b4
abigmiu <1552487872@qq.com>
2021-10-11 23:29:34 +0800

    xxx

--- net_music/src/App.js
+++ net_music/src/App.js
@@ -4,12 +4,14 @@ import React from "react";
 import { HashRouter } from "react-router-dom";
 
 import AppHeader from "./components/app-header";
+import AppFooter from "./components/app-footer";
 
 export default function App() {
   return (
     <>
       <HashRouter>
         <AppHeader></AppHeader>
+        <AppFooter></AppFooter>
       </HashRouter>
     </>
   );
