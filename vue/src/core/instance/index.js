18fdb6ee0c40f3aa67f5a8ef93bb194ea191449d
abigmiu <1552487872@qq.com>
2021-11-24 00:44:55 +0800

    进步一点点

--- vue/src/core/instance/index.js
+++ vue/src/core/instance/index.js
@@ -1,23 +1,23 @@
-import { initMixin } from './init'
-import { stateMixin } from './state'
-import { renderMixin } from './render'
-import { eventsMixin } from './events'
-import { lifecycleMixin } from './lifecycle'
-import { warn } from '../util/index'
+import { initMixin } from "./init";
+import { stateMixin } from "./state";
+import { renderMixin } from "./render";
+import { eventsMixin } from "./events";
+import { lifecycleMixin } from "./lifecycle";
+import { warn } from "../util/index";
 
+// 这里只是定义， 不执行
 function Vue(options) {
-  if (process.env.NODE_ENV !== 'production' &&
-    !(this instanceof Vue)
-  ) {
-    warn('Vue is a constructor and should be called with the `new` keyword')
+  if (process.env.NODE_ENV !== "production" && !(this instanceof Vue)) {
+    warn("Vue is a constructor and should be called with the `new` keyword");
   }
-  this._init(options)
+  this._init(options);
 }
 
-initMixin(Vue)
-stateMixin(Vue)
-eventsMixin(Vue)
-lifecycleMixin(Vue)
-renderMixin(Vue)
+// 先执行这句
+initMixin(Vue);
+stateMixin(Vue);
+eventsMixin(Vue);
+lifecycleMixin(Vue);
+renderMixin(Vue);
 
-export default Vue
+export default Vue;
--- vue/src/core/instance/init.js
+++ vue/src/core/instance/init.js
@@ -1,123 +1,141 @@
 /* @flow */
 
-import config from '../config'
-import { initProxy } from './proxy'
-import { initState } from './state'
-import { initRender } from './render'
-import { initEvents } from './events'
-import { mark, measure } from '../util/perf'
-import { initLifecycle, callHook } from './lifecycle'
-import { initProvide, initInjections } from './inject'
-import { extend, mergeOptions, formatComponentName } from '../util/index'
+import config from "../config";
+import { initProxy } from "./proxy";
+import { initState } from "./state";
+import { initRender } from "./render";
+import { initEvents } from "./events";
+import { mark, measure } from "../util/perf";
+import { initLifecycle, callHook } from "./lifecycle";
+import { initProvide, initInjections } from "./inject";
+import { extend, mergeOptions, formatComponentName } from "../util/index";
 
-let uid = 0
+let uid = 0;
 
 export function initMixin(Vue: Class<Component>) {
   Vue.prototype._init = function (options?: Object) {
-    const vm: Component = this
+    // vm实例
+    const vm: Component = this;
     // a uid
-    vm._uid = uid++
+    vm._uid = uid++;
 
-    let startTag, endTag
+    let startTag, endTag; // 性能计算
     /* istanbul ignore if */
-    if (process.env.NODE_ENV !== 'production' && config.performance && mark) {
-      startTag = `vue-perf-start:${vm._uid}`
-      endTag = `vue-perf-end:${vm._uid}`
-      mark(startTag)
+    if (process.env.NODE_ENV !== "production" && config.performance && mark) {
+      startTag = `vue-perf-start:${vm._uid}`;
+      endTag = `vue-perf-end:${vm._uid}`;
+      mark(startTag);
     }
 
+    // note:避免被监测
     // a flag to avoid this being observed
-    vm._isVue = true
+    vm._isVue = true;
+
+    // NOTE: 合并选项
     // merge options
+    // DEBUG:
+    console.warn("initMixin");
+    console.log(options);
     if (options && options._isComponent) {
       // optimize internal component instantiation
       // since dynamic options merging is pretty slow, and none of the
       // internal component options needs special treatment.
-      initInternalComponent(vm, options)
+      initInternalComponent(vm, options);
     } else {
+      // NOTE: 走这一步
+      // DEBUG:
+      console.log("step1 mergeOptions");
+      console.log(vm);
       vm.$options = mergeOptions(
         resolveConstructorOptions(vm.constructor),
         options || {},
         vm
-      )
+      );
     }
     // 拦截 ，得到警告信息
     /* istanbul ignore else */
-    if (process.env.NODE_ENV !== 'production') {
-      initProxy(vm)
+    if (process.env.NODE_ENV !== "production") {
+      initProxy(vm);
     } else {
-      vm._renderProxy = vm
+      vm._renderProxy = vm;
     }
     // expose real self
-    vm._self = vm
+    vm._self = vm;
     // 生命周期相关变量
-    initLifecycle(vm)
+    initLifecycle(vm);
     // 事件监听初始化
-    initEvents(vm)
-    initRender(vm)
-    callHook(vm, 'beforeCreate')
-    initInjections(vm) // resolve injections before data/props
+    initEvents(vm);
+    initRender(vm);
+    callHook(vm, "beforeCreate");
+    initInjections(vm); // resolve injections before data/props
     // vm的状态初始化，prop/data/computed/method/watch都在这里完成初始化，因此也是Vue实例create的关键。
-    initState(vm)
-    initProvide(vm) // resolve provide after data/props
-    callHook(vm, 'created')
+    initState(vm);
+    initProvide(vm); // resolve provide after data/props
+    callHook(vm, "created");
 
     /* istanbul ignore if */
-    if (process.env.NODE_ENV !== 'production' && config.performance && mark) {
-      vm._name = formatComponentName(vm, false)
-      mark(endTag)
-      measure(`vue ${vm._name} init`, startTag, endTag)
+    if (process.env.NODE_ENV !== "production" && config.performance && mark) {
+      vm._name = formatComponentName(vm, false);
+      mark(endTag);
+      measure(`vue ${vm._name} init`, startTag, endTag);
     }
 
     if (vm.$options.el) {
-      vm.$mount(vm.$options.el)
+      vm.$mount(vm.$options.el);
     }
-  }
+  };
 }
 
-export function initInternalComponent(vm: Component, options: InternalComponentOptions) {
-  const opts = vm.$options = Object.create(vm.constructor.options)
+export function initInternalComponent(
+  vm: Component,
+  options: InternalComponentOptions
+) {
+  const opts = (vm.$options = Object.create(vm.constructor.options));
   // doing this because it's faster than dynamic enumeration.
-  const parentVnode = options._parentVnode
-  opts.parent = options.parent
-  opts._parentVnode = parentVnode
+  const parentVnode = options._parentVnode;
+  opts.parent = options.parent;
+  opts._parentVnode = parentVnode;
 
-  const vnodeComponentOptions = parentVnode.componentOptions
-  opts.propsData = vnodeComponentOptions.propsData
-  opts._parentListeners = vnodeComponentOptions.listeners
-  opts._renderChildren = vnodeComponentOptions.children
-  opts._componentTag = vnodeComponentOptions.tag
+  const vnodeComponentOptions = parentVnode.componentOptions;
+  opts.propsData = vnodeComponentOptions.propsData;
+  opts._parentListeners = vnodeComponentOptions.listeners;
+  opts._renderChildren = vnodeComponentOptions.children;
+  opts._componentTag = vnodeComponentOptions.tag;
 
   if (options.render) {
-    opts.render = options.render
-    opts.staticRenderFns = options.staticRenderFns
+    opts.render = options.render;
+    opts.staticRenderFns = options.staticRenderFns;
   }
 }
 
 export function resolveConstructorOptions(Ctor: Class<Component>) {
   // Ctor vue构造函数
-  let options = Ctor.options
-
+  let options = Ctor.options;
+  // DEBUG:
+  console.warn("resolveConstructorOptions");
+  console.log(Ctor);
   // 针对Vue.extends()
   if (Ctor.super) {
-    const superOptions = resolveConstructorOptions(Ctor.super)
-    const cachedSuperOptions = Ctor.superOptions
+    console.log("Ctor-super");
+    const superOptions = resolveConstructorOptions(Ctor.super);
+    const cachedSuperOptions = Ctor.superOptions;
     if (superOptions !== cachedSuperOptions) {
       // super option changed,
       // need to resolve new options.
-      Ctor.superOptions = superOptions
+      Ctor.superOptions = superOptions;
       // check if there are any late-modified/attached options (#4976)
-      const modifiedOptions = resolveModifiedOptions(Ctor)
+      const modifiedOptions = resolveModifiedOptions(Ctor);
       // update base extend options
       if (modifiedOptions) {
-        extend(Ctor.extendOptions, modifiedOptions)
+        extend(Ctor.extendOptions, modifiedOptions);
       }
-      options = Ctor.options = mergeOptions(superOptions, Ctor.extendOptions)
+      options = Ctor.options = mergeOptions(superOptions, Ctor.extendOptions);
       if (options.name) {
-        options.components[options.name] = Ctor
+        options.components[options.name] = Ctor;
       }
     }
   }
+  console.log(options);
   /**
    * @returns options
    * components: {KeepAlive: {…}, Transition: {…}, TransitionGroup: {…}}
@@ -126,18 +144,18 @@ export function resolveConstructorOptions(Ctor: Class<Component>) {
     _base: ƒ Vue(options)
 
    */
-  return options
+  return options;
 }
 
 function resolveModifiedOptions(Ctor: Class<Component>): ?Object {
-  let modified
-  const latest = Ctor.options
-  const sealed = Ctor.sealedOptions
+  let modified;
+  const latest = Ctor.options;
+  const sealed = Ctor.sealedOptions;
   for (const key in latest) {
     if (latest[key] !== sealed[key]) {
-      if (!modified) modified = {}
-      modified[key] = latest[key]
+      if (!modified) modified = {};
+      modified[key] = latest[key];
     }
   }
-  return modified
+  return modified;
 }
